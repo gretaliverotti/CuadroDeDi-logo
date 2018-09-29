1a86c28c6fccda449d81bd83a13fec7633f2f1f1
Greta <Oscar@Equipo>
2018-09-29 18:31:18 -0300

    Ejercicio Java sobre Ventanas y parseInt.

---
+++ src/cuadrodedialogo/CuadroDeDialogo.java
@@ -0,0 +1,221 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cuadrodedialogo;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Greta
+ */
+public class CuadroDeDialogo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        JOptionPane.showMessageDialog(null, "            ¡Bienvenidos!");
+        String nombre = JOptionPane.showInputDialog("Ingrese su nombre: ");
+        JOptionPane.showMessageDialog(null, "             Hola " + nombre);
+        
+      
+        String n1 = JOptionPane.showInputDialog("Ingrese un número entero para sumarlo: ");
+        System.out.println("El número ingresados es: " + n1);
+        String n2 = JOptionPane.showInputDialog("Ingrese otro número entero: ");
+        System.out.println("El número ingresados es: " + n2);
+        
+        int num1 = Integer.parseInt(n1);
+        int num2 = Integer.parseInt(n2);
+        
+        int suma = num1 + num2;
+        
+        System.out.println("El resultado de la suma es: " + suma);
+        
+        JOptionPane.showMessageDialog(null, "El resultado de la suma es: " + suma);
+        
+   
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+}
